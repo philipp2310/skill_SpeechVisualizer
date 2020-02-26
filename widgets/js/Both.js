5c4da0062c8ddc19cc42956f4ff0d46f2da1b02e
Philipp <41761223+philipp2310@users.noreply.github.com>
2020-02-27 00:58:00 +0100

    graphics and more info

--- widgets/js/Both.js
+++ widgets/js/Both.js
@@ -3,15 +3,29 @@ function onConnect(){
 	mqtt.subscribe("hermes/asr/partialTextCaptured")
 	mqtt.subscribe("hermes/asr/textCaptured")
 	mqtt.subscribe("hermes/tts/say")
+	mqtt.subscribe("hermes/nlu/intentParsed")
+	mqtt.subscribe("hermes/asr/stopListening")
+	mqtt.subscribe("hermes/asr/startListening")
 }
 
-function onMessage(msg){
+function onMessage(msg) {
 	//msg to json, get 'text'
-	console.log(msg.payloadString);
 	let json = JSON.parse(msg.payloadString);
-	if(msg.destinationName == 'hermes/asr/partialTextCaptured' || msg.destinationName == 'hermes/asr/textCaptured') {
+	if (msg.destinationName == 'hermes/asr/partialTextCaptured') {
+		$('#ASRcontentBoth')[0].innerHTML = json['text'] + " ...";
+		$('#ASRcontentInput')[0].innerHTML = json['text'] + " ...";
+		$('#ASRconf')[0].innerHTML = Number.parseFloat(json['likelihood']).toPrecision(2)*100 + "%";
+	} else if (msg.destinationName == 'hermes/asr/textCaptured') {
 		$('#ASRcontentBoth')[0].innerHTML = json['text'];
 		$('#ASRcontentInput')[0].innerHTML = json['text'];
+		$('#ASRconf')[0].innerHTML = Number.parseFloat(json['likelihood']).toPrecision(2)*100 + "%";
+	} else if (msg.destinationName == 'hermes/nlu/intentParsed') {
+		$('#NLUskill')[0].innerHTML = json['intent']['intentName'];
+		$('#NLUconf')[0].innerHTML = Number.parseFloat(json['intent']['confidenceScore']).toPrecision(2)*100 + "%";
+	} else if (msg.destinationName == 'hermes/asr/stopListening'){
+		$('#SVI_ICON').attr('class', 'fas fa-microphone-slash');
+	} else if (msg.destinationName == 'hermes/asr/startListening') {
+		$('#SVI_ICON').attr('class', 'fas fa-microphone');
 	} else {
 		$('#ASRcontentBoth')[0].innerHTML = json['text'];
 		$('#ASRcontentOutput')[0].innerHTML = json['text'];
