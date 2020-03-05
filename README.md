b4ec083d70f960b7382e9423d484f8fbdaef2b33
Psycho <laurentchervet@bluewin.ch>
2020-03-05 17:26:50 +0100

    Moved mosquitto websocket config to initializer

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ### Description
 Shows the voice in and output as a widget
 
-- Version: 0.0.6
+- Version: 0.0.7
 - Author: philipp2310
 - Maintainers: Psychokiller1888
 - Alice minimum Version: 1.0.0-a6
--- SpeechVisualizer.py
+++ SpeechVisualizer.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from core.base.model.AliceSkill import AliceSkill
 
 
@@ -7,11 +5,3 @@ class SpeechVisualizer(AliceSkill):
 
 	def __init__(self):
 		super().__init__()
-
-
-	def onStart(self):
-		super().onStart()
-		confPath = Path('/etc/mosquitto/conf.d/websockets.conf')
-		if not confPath.exists():
-			self.Commons.runRootSystemCommand(['cp', 'skills/SpeechVisualizer/websockets.conf', str(confPath)])
-			self.Commons.runRootSystemCommand(['systemctl', 'restart', 'mosquitto'])
